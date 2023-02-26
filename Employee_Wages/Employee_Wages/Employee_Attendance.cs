102ce52bddfbb1bcfc1382ff180d116b8c3662fa
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-26 17:16:24 -0500

    [Ruchita] Add: Employee Attendance is calculated

---
+++ Employee_Wages/Employee_Wages/Employee_Attendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wages
+{
+    internal class Employee_Attendance
+    {
+        public static void EmployeeAttendancecheck()
+        {
+            int is_full_time = 1;
+
+            Random rnd = new Random();
+            int emp_check = rnd.Next(0, 2);
+            if (is_full_time == emp_check)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Abscent");
+            }
+        }
+    }
+}
---
+++ Employee_Wages/Employee_Wages/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_Wages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages Program Computation");
+            Employee_Attendance.EmployeeAttendancecheck();
+        }
+    }
+}
